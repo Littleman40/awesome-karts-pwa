c0e5c41b294aaeb33e329f7be45d286fa8e86287
Littleman40 <223826819+Littleman40@users.noreply.github.com>
2026-05-13 13:52:19 +1000

    small comment update

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {                                    // loads service worker to allow for pwa and caching
     window.addEventListener("load", () => {
         navigator.serviceWorker
             .register("/sw.js")
